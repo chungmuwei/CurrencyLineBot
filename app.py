49fbb19668a205bebb44cce95019a4dc792dc4bc
Mu-Wei Chung <raymondc0302@gmail.com>
2023-04-08 09:08:21 +1000

    try to deploy

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 # '0FmaYmoBD+O1kPOtna88s5uehDgVbGBNQaoFHA6L1nBOsunQMYg5vVjPltt57j+/uH305Uys6wcmXTxjAb2QBDQwF0/68WSwIWmUl9TfIT4jKIx+3LIIdn6TbBKAbMoaAlt2OkUkWjG2QuNDQuCBLQdB04t89/1O/w1cDnyilFU='
 # 'ce8c604a308615d0f101c2bbbbab1e5f'
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
 
 # https://a0bf-27-33-126-123.ngrok.io/callback
 
@@ -70,24 +70,28 @@ def currency_rate_report(currency):
 
 
 
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info(body)
+    if request.method == 'GET':
+        return "Hello Heroku"
     
+    if request.method == 'POST':
+        # get X-Line-Signature header value
+        signature = request.headers['X-Line-Signature']
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+        # get request body as text
+        body = request.get_data(as_text=True)
+        app.logger.info(body)
+        
 
-    return 'OK'
+        # handle webhook body
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            print("Invalid signature. Please check your channel access token/channel secret.")
+            abort(400)
+
+        return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
